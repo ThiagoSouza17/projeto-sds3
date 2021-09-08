6c97d333559b24ee3a5c54198cfa15cd9acaa3b0
Thiago Souza <thiagossouza07@gmail.com>
2021-09-08 18:32:00 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/thiago-dos-santos-souza-b4183553/" target="_blank" rel="noreferrer">Thiago Souza</a><br />
+                    Acesse o <a href="https://github.com/ThiagoSouza17" target="_blank" rel="noreferrer"> meu Github</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
